9e7afa4c49ca3031f86222fadb27ea56cb69e707
Antoine Guérin <90921460+AntGuerin23@users.noreply.github.com>
2022-10-28 15:30:07 -0400

    Project Setup

---
+++ Trackeo/Shared/TrackeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackeoApp.swift
+//  Shared
+//
+//  Created by Réjean on 2022-10-28.
+//
+
+import SwiftUI
+
+@main
+struct TrackeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
