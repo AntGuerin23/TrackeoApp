db94c9cd4a358ce3e47dc25c57de2dffd753fa11
Antoine Guérin <90921460+AntGuerin23@users.noreply.github.com>
2022-11-28 15:27:31 -0500

    Feat: Can now calculate distance between two points

--- Trackeo/Shared/Views/CompassView.swift
+++ Trackeo/Shared/Views/CompassView.swift
@@ -2,7 +2,11 @@ import SwiftUI
 
 struct CompassView: View {
     var body: some View {
-        Text("oafeioaefmw!")
+        HStack {
+            
+        }.task {
+            print(LocationCalculator.calculateDistance( firstPoint: Location(id: UUID(), latitude: 46.03990770130233, longitude: -73.12967918789305), secondPoint: Location(id: UUID(), latitude: 46.039668691550595, longitude: -73.14779880378347)))
+        }
     }
 }
 
